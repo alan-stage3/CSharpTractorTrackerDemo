9dcdecce4c507a260fa438ca8a45f461b4c64064
Alan Galloway <alan@stage3talent.com>
2022-04-27 14:58:59 -0400

    First Break - Add MVC and Driver Controller

---
+++ TractorTracker.MVC/Controllers/DriversController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TractorTracker.Core;
+using TractorTracker.Core.Entities;
+using TractorTracker.Core.Interfaces;
+
+namespace TractorTracker.MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private readonly IDriverRepository _driverRepo;
+
+        public DriversController(IDriverRepository driverRepo)
+        {
+            _driverRepo = driverRepo;
+        }
+
+        [HttpGet]
+        public IActionResult GetDrivers()
+        {
+            Result<List<Driver>> result = _driverRepo.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Messages);
+            }
+
+        }
+
+    }
+}
